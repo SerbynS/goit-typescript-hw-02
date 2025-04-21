5fbdb1c4545d988cdc359d39e6fc6519c9b79526
SerbynS <serbin.sasha1@gmail.com>
2025-04-21 23:01:50 +0300

    Create project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import ImageGallery from "../ImageGallery/ImageGallery";
+import ImageModal from "../ImageModal/ImageModal";
+import Loader from "../Loader/Loader";
+import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "../SearchBar/SearchBar";
+
+import css from "./App.module.css";
+
+export default function App() {
+  return <></>;
+}
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,7 @@
+import ImageCard from "./ImageCard/ImageCard";
+
+import css from "./ImageGallery.module.css";
+
+export default function ImageGallery() {
+  return <></>;
+}
