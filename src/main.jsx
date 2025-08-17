e9c1e2be14b574668d7078f9c6b15b847882b89e
SerbynS <serbin.sasha1@gmail.com>
2025-08-17 21:45:32 +0300

    rename files

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "modern-normalize";
-
-import "./index.css";
-import App from "./components/App/App.jsx";
-
-createRoot(document.getElementById("root")).render(<App />);
